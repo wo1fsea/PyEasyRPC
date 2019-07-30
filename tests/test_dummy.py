1fa98efb9038214e2340d68d57860a1f00897347
wo1fsea <wo1fsea@126.com>
2019-07-30 15:37:50 +0800

    add: CI

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -6,14 +6,14 @@ Author:
 Date:
     2019/7/30
 Description:
-    DummyTest.py
+    test_dummy.py
 ----------------------------------------------------------------------------"""
 import unittest
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        self.assertEqual(True, True)
 
 
 if __name__ == '__main__':
